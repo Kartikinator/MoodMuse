85508fc4f66aab32037f2a326aaf2a4ef1f68456
Abdullah Sharif <abdullah.z.sharif@gmail.com>
2025-03-30 16:36:05 -0500

    newfront final copy like actually tho

--- moodmuse/src/components/Header.tsx
+++ moodmuse/src/components/Header.tsx
@@ -187,7 +187,7 @@ const Header: React.FC = () => {
         
         <ProfileButton onClick={toggleMenu}>
           <div className="avatar">JD</div>
-          <span className="name">John Doe</span>
+          <span style = {{color: "white"}}className="name">John Doe</span>
           <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M7 10L12 15L17 10H7Z" fill="currentColor" />
           </svg>
--- moodmuse/src/components/MoodDetector.tsx
+++ moodmuse/src/components/MoodDetector.tsx
@@ -339,12 +339,12 @@ const MoodDetector: React.FC<MoodDetectorProps> = ({ onMoodDetected }) => {
           {isCameraVisible ? 'Hide Camera' : 'Show Camera'}
         </Button>
         
-        <StatsButton onClick={toggleStats}>
-          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <Button onClick={toggleStats}>
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '8px' }}>
             <path d="M19 3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3ZM9 17H7V10H9V17ZM13 17H11V7H13V17ZM17 17H15V13H17V17Z" fill="currentColor" />
           </svg>
-          {isStatsVisible ? 'Hide Statistics' : 'Show Statistics'}
-        </StatsButton>
+          {isStatsVisible ? 'Mood Statistics' : 'Mood Statistics'}
+        </Button>
       </ButtonsContainer>
       
       <CameraContainer isVisible={isCameraVisible}>
